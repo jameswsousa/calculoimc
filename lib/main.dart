a4d81b0dc3d5611da2cd334919192966cd0d4206
James Sousa <jameswsousa.eng@gmail.com>
2020-10-27 15:05:05 -0400

    improves usability

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     theme: ThemeData(primarySwatch: Colors.pink),
     home: HomeWidget(),
   ));
@@ -46,7 +47,7 @@ class _HomeWidgetState extends State<HomeWidget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.yellow,
+      backgroundColor: Colors.yellowAccent,
       appBar: AppBar(
         centerTitle: true,
         title: Text("Calculadora de IMC"),
@@ -60,88 +61,102 @@ class _HomeWidgetState extends State<HomeWidget> {
           )
         ],
       ),
-      body: Form(key: _formkey,
+      body: Form(
+        key: _formkey,
         child: Container(
-          margin: EdgeInsets.all(20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Icon(
-                Icons.person_outline,
-                size: 150,
-                color: Colors.pink,
-              ),
-              TextFormField(
-                cursorColor: Colors.pink,
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return "Insira seu peso";
-                  }
-                },
-                controller: weightController,
-                keyboardType: TextInputType.number,
-                autofocus: false,
-                textAlign: TextAlign.center,
-                style: TextStyle(color: Colors.pink, fontSize: 25),
-                decoration: InputDecoration(
-                    enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.pink)),
-                        focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.pink)),
-                        border: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.pink)),
-                    labelText: "Peso (Kg)",
-                    labelStyle: TextStyle(color: Colors.pink, fontSize: 20)),
-              ),
-              TextFormField(
-                validator: (value) {
-                  if (value.isEmpty) {
-                    return "Insira sua altura";
-                  }
-                },
-                controller: heightController,
-                keyboardType: TextInputType.number,
-                autofocus: false,
-                textAlign: TextAlign.center,
-                cursorColor: Colors.pink,
-                style: TextStyle(color: Colors.pink, fontSize: 25),
-                decoration: InputDecoration(
-                   enabledBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.pink)),
-                        focusedBorder: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.pink)),
-                        border: UnderlineInputBorder(
-                        borderSide: BorderSide(color: Colors.pink)),
-                    labelText: "Altura (cm)",
-                    labelStyle: TextStyle(color: Colors.pink, fontSize: 20)),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              RaisedButton(
-                onPressed: () {
-                  if (_formkey.currentState.validate()) {
-                    calculate();
-                  }
-                },
-                color: Colors.pink,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    "Calcular",
-                    style: TextStyle(fontSize: 25, color: Colors.white),
+          margin: EdgeInsets.all(30),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Icon(
+                  Icons.person_outline,
+                  size: 150,
+                  color: Colors.pink,
+                ),
+                TextFormField(
+                  cursorColor: Colors.pink,
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return "Insira seu peso";
+                    }
+                  },
+                  controller: weightController,
+                  keyboardType: TextInputType.number,
+                  autofocus: false,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.pink, fontSize: 25),
+                  decoration: InputDecoration(
+                      hintText: "Digite seu peso",
+                      hintStyle: TextStyle(color: Colors.pink[200]),
+                      enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.pink)),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.pink)),
+                      border: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.pink)),
+                      labelText: "Peso (Kg)",
+                      labelStyle: TextStyle(color: Colors.pink, fontSize: 20)),
+                ),
+                TextFormField(
+                  validator: (value) {
+                    if (value.isEmpty) {
+                      return "Insira sua altura";
+                    }
+                  },
+                  controller: heightController,
+                  keyboardType: TextInputType.number,
+                  
+                  autofocus: false,
+                  textAlign: TextAlign.center,
+                  cursorColor: Colors.pink,
+                  style: TextStyle(color: Colors.pink, fontSize: 25),
+                  decoration: InputDecoration(
+                      hintText: "Digite sua altura(em cm)",
+                      hintStyle: TextStyle(color: Colors.pink[200]),
+                      enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.pink)),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.pink)),
+                      border: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.pink)),
+                      labelText: "Altura (cm)",
+                      labelStyle: TextStyle(color: Colors.pink, fontSize: 20)),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                GestureDetector(
+                  onTap: () {
+                    if (_formkey.currentState.validate()) {
+                      calculate();
+                    }
+                  },
+                  child: Container(
+                    margin: EdgeInsets.all(5),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(30),
+                      boxShadow: <BoxShadow>[BoxShadow(offset: Offset(3, 3), blurRadius: 3,color: Colors.pink[800])],
+                      color: Colors.pink,
+                    ),
+                    padding: EdgeInsets.symmetric(vertical: 15, horizontal: 20),
+                    child: Text(
+                      "Calcular",
+                      style: TextStyle(fontSize: 25, color: Colors.white),
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Text(
-                _infoText,
-                style: TextStyle(fontSize: 25, color: Colors.pink),
-                textAlign: TextAlign.center,
-              )
-            ],
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  _infoText,
+                  style: TextStyle(fontSize: 25, color: Colors.pink),
+                  textAlign: TextAlign.center,
+                )
+              ],
+            ),
           ),
         ),
       ),
